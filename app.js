2135c22a5d4ddfb8bd05cb298c3250b181c9183e
Dennis Oliver Oellrich <dennis.oellrich@gmail.com>
2022-02-21 17:00:46 +0100

    test 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Hallo Rene, CICD geht');
+  res.write('Hallo Rene, CICD geht und wie');
   res.end();
 }).listen(8080, '0.0.0.0');
